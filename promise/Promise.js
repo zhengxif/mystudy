9987d1e8d0f92032340615ca67e554e712d726b5
郑玺 <dongsun.zx@raycloud.com>
2019-09-23 19:54:10 +0800

    promise

--- promise/Promise.js
+++ promise/Promise.js
@@ -1,164 +1,83 @@
-const PENGDING = 'pengding';
-        const FULFILLED = 'fulfilled';
-        const REJECTED = 'rejected';
-        class MyPromise {
-            constructor(handler) {
-                this.status = PENGDING;
-                this.value = undefined;
-                // 成功的队列
-                this.fulfilledQueue = [];
-                // 失败的队列
-                this.rejectedQueue = [];
-                try {
-                    handler(this.resolve.bind(this), this.reject.bind(this))
-                } catch (error) {
-                    this.reject(error);
-                }
-            }
-            resolve(value) {
-                const run = () => {
-                    if (this.status !== PENGDING) return;
-                    const runFulfilled = value => {
-                        let cb;
-                        while(cb = this.fulfilledQueue.shift()) {
-                            cb(value);
-                        }
-                    }
-                    const runRejected = value => {
-                        let cb;
-                        while(cb = this.rejectedQueue.shift()) {
-                            cb(value);
-                        }
-                    }
-                    if (value instanceof MyPromise) {
-                       value.then(value => {
-                           this.value = value;
-                           this.status = FULFILLED;
-                           runFulfilled(value);
-                       })
-                    } else {
-                        this.value = value; 
-                        this.status = FULFILLED;
-                        runFulfilled(value);
-                    }
-                }
 
-                setTimeout(run, 0)
-            }
-            reject(value) {
-                const run = () => {
-                    if(this.status !== PENGDING) return;
-                    this.status = REJECTED;
-                    this.value = value;
-                    let cb;
-                    while(cb = this.rejectedQueue.shift()) {
-                        cb(value);
-                    }
-                }
-                setTimeout(run, 0)
-            }
-            then(onFulfilled, onRejected) {
-                const { status, value } = this;
-                return new MyPromise((onFulfilledNext, onRejectedNext) => {
-                    // 成功的回调队列项
-                    const fulfilled = value => {
-                        try {
-                            if (typeof onFulfilled !== 'function') {
-                                onFulfilledNext(value);
-                            } else {
-                                let res = onFulfilled(value);
-                                if (res instanceof MyPromise) {
-                                    res.then(value => {
-                                        onFulfilledNext(value);
-                                    }, error => {
-                                        onRejectedNext(error);
-                                    })
-                                } else {
-                                    onFulfilledNext(value);
-                                }
-                            }
-                        } catch (error) {
-                            onRejectedNext(error);
-                        }
-                    }
-                    //是失败的回调队列项
-                    const rejected = error => {
-                        try {
-                            if (typeof onRejected !== 'function') {
-                                onRejectedNext(error);
-                            }else {
-                                let res = onRejected(error);
-                                if (res instanceof MyPromise) {
-                                    res.then(onFulfilledNext, onRejectedNext);
-                                }else {
-                                    onFulfilledNext(error);
-                                }
-                            }
-                        } catch (error) {
-                            onRejectedNext(error);
-                        }
-                    }
-                    switch (status) {
-                        case PENGDING:
-                            this.fulfilledQueue.push(fulfilled);
-                            this.rejectedQueue.push(rejected);
-                            break;
-                        case FULFILLED:
-                            break;
-                        case REJECTED:
-                            break;
-                    }
-                }) 
-            }
-            catch(onRejected) {
-                return this.then(undefined, onRejected);
-            }
-            static resolve(value) {
-                if (value instanceof MyPromise) return value;
-                return new MyPromise(resolve => resolve(value))
-            }
-            static reject(error) {
-                return new MyPromise((resolve, reject) => reject(error))
-            }
-            static all(list) {
-                return new MyPromise((resolve, reject) => {
-                    let count = 0;
-                    let arr = [];
-                    for(let [i, p] of list.entrise()) {
-                        this.resolve(p).then(value => {
-                            if (count == list.length)  resolve(arr);
-                            count ++;
-                            arr[i] = value;
-                        }, error => {
-                            reject(error);
-                        })
-                    }
-                })
-            }
-            static race(list) {
-                return new MyPromise((resolve, reject) => {
-                    for( let p of list) {
-                        this.resolve(p).then(value => {
-                            resolve(value);
-                        }, error => {
-                            reject(error)
-                        })
-                    }
-                })
-            }
-            finally(cb) {
-                return this.then((value) => {
-                    MyPromise.resolve(cb()).then(() => value)
-                }, reason => {
-                    MyPromise.resolve(cb()).then(() => error)
-                })
-            }
+const PENGDING = 'PENGDING';
+const FULFILLED = 'FULFILLED';
+const REJECTED = 'REJECTED';
+class Promise {
+    constructor(executor) {
+        debugger
+        this.status = PENGDING;
+        this.value = undefined;
+        this.reason = undefined;
+        this.resolveCallBack = [];
+        this.rejectCallBack = [];
+        let resolve = value => {
+            this.status = FULFILLED;
+            this.value = value;
+            this.resolveCallBack.forEach(fn => fn());
         }
+        let reject = reason => {
+            this.status = REJECTED;
+            this.reason = reason;
+            this.rejectCallBack.forEach( fn => fn());
+        }
+        try {
+            executor(resolve, reject);
+        } catch (error) {
+            reject(error);
+        }
+    }
+    then(onFulfilled, onRejected) {
+        let promise2 = new Promise((reoslve, reject) => {
+            // 如果上个promise状态是FUlFILLED, 那么直接执行then中onFulfilled,并调用resolve
+            if (this.status == FULFILLED) {
+                setTimeout(() => {
+                    try {
+                        let x = onFulfilled(this.value);
+                        resolve(x);
+                    } catch (error) {
+                        reject(error);
+                    }
+                })
+            }
+            // 如果上个promise状态是REJECTED, 那么直接执行then中onRejected,并调用reject
+            if (this.status == REJECTED) {
+                setTimeout(() => {
+                    try {
+                        let x = onRejected(this.reason);
+                        resolve(x);
+                    } catch (error) {
+                        reject(error);
+                    }
+                })
+            }
+            if (this.status == PENGDING) {
+                this.resolveCallBack.push(() => {
+                    setTimeout(() => {
+                        try {
+                            let x = onFulfilled(this.value);
+                            resolve(x);
+                        } catch (error) {
+                            reject(error);
+                        }
+                    })
+                })
+                this.reejctCallBack.push(() => {
+                    setTimeout(() => {
+                        try {
+                            let x = onRejected(this.reason);
+                            resolve(x);
+                        } catch (error) {
+                            reject(error);
+                        }
+                    })
+                })
+            }
 
-        new MyPromise((resolve, reject) => {
-            reject(5)
-        }).then(value => {
-            console.log(value)
-        }).catch(error => {
-            console.log(error)
         })
+        return promise2;
+    }
+}
+
+new Promise((resolve, reject) => {
+    resolve(1)
+}).then(data => console.log(data))
